60d8ddaa53643c4bf630ff60caf3922b1a909143
Jefferson Lopes <jeffbreno@gmail.com>
2024-09-23 14:14:17 -0300

    Atualizando logica de botão de estilos

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,19 @@
+import { ThemeProvider } from 'styled-components'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { GlobalStyle } from './styles/global'
-import { Search } from './pages/Search'
-import { Books } from './pages/Books'
-import { BooksDetail } from './pages/BookDetail'
+import { theme } from './styles/theme'
+import { SignIn } from './pages/Auth/Signin'
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyle />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Search />} />
-          <Route path="/books" element={<Books />} />
-          <Route path="/books/:bookId" element={<BooksDetail />} />
+          <Route path="/" element={<SignIn />} />
         </Routes>
       </BrowserRouter>
-    </>
+    </ThemeProvider>
   )
 }
 
---
+++ src/pages/Auth/Signin/SignIn.tsx
@@ -0,0 +1,12 @@
+import { Button } from '../../../components/Button'
+
+export function SignIn() {
+  return (
+    <div style={{ width: 600 }}>
+      <h1>Faça seu Login</h1>
+      <Button fullWidth>
+        Entrar
+      </Button>
+    </div>
+  )
+}
